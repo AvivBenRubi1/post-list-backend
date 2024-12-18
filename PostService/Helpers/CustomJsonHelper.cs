f441b03f0185c56cefdcc038e6d4cd89f649912b
Aviv Ben Rubi <avivbenrubi1@gmail.com>
2024-12-18 23:56:18 +0200

    add SemaphoreSlim to JsonHelper

--- PostService/Helpers/CustomJsonHelper.cs
+++ PostService/Helpers/CustomJsonHelper.cs
@@ -7,19 +7,41 @@ namespace PostService.Helpers
     public class CustomJsonHelper : ICustomJsonHelper
     {
         private static readonly string FilePath = Path.Combine(Directory.GetCurrentDirectory(), "posts.json");
+
+        // Semaphore to limit access to the file
+        private static readonly SemaphoreSlim fileSemaphore = new(1, 1);
+
         public async Task<IEnumerable<T>> Read<T>()
         {
-            using StreamReader file = File.OpenText(FilePath);
-            var json = await file.ReadToEndAsync();
-            return JsonConvert.DeserializeObject<IEnumerable<T>>(json) ?? [];
+            await fileSemaphore.WaitAsync(); // Wait for the lock
+
+            try
+            {
+                using StreamReader file = File.OpenText(FilePath);
+                var json = await file.ReadToEndAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<T>>(json) ?? Enumerable.Empty<T>();
+            }
+            finally
+            {
+                fileSemaphore.Release(); // Release the lock
+            }
         }
 
         public async Task Write<T>(IEnumerable<T> data)
         {
-            using StreamWriter file = new(FilePath, false);
-            JsonSerializer serializer = new();
-            await Task.Yield(); // Yield to avoid blocking the thread
-            serializer.Serialize(file, data);
+            await fileSemaphore.WaitAsync(); // Wait for the lock
+
+            try
+            {
+                using StreamWriter file = new(FilePath, false);
+                JsonSerializer serializer = new();
+                await Task.Yield(); // Yield to avoid blocking the thread
+                serializer.Serialize(file, data);
+            }
+            finally
+            {
+                fileSemaphore.Release(); // Release the lock
+            }
         }
     }
 }
